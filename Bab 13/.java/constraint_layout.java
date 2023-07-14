7330843f2ac26ace4d02c917acf81d5d6c5aa2d2
Jul's <57309553+MyLovelyKitten@users.noreply.github.com>
2023-07-14 22:38:31 +0700

    Update constraint_layout.java

--- Bab 13/.java/constraint_layout.java
+++ Bab 13/.java/constraint_layout.java
@@ -1,1 +1,33 @@
+import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout constraintLayout;
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        constraintLayout = findViewById(R.id.constraint_layout);
+        textView = findViewById(R.id.text_view);
+        button = findViewById(R.id.button);
+
+        ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
+                ConstraintLayout.LayoutParams.WRAP_CONTENT,
+                ConstraintLayout.LayoutParams.WRAP_CONTENT
+        );
+        params.startToStart = ConstraintLayout.LayoutParams.PARENT_ID;
+        params.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
+        params.endToEnd = ConstraintLayout.LayoutParams.PARENT_ID;
+        params.bottomToTop = button.getId();
+        textView.setLayoutParams(params);
+    }
+}
 
