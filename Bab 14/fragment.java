503e413aa2c84256a499b79666211df836d43d0c
Jul's <57309553+MyLovelyKitten@users.noreply.github.com>
2023-07-14 22:34:50 +0700

    Update fragment.java

--- Bab 14/fragment.java
+++ Bab 14/fragment.java
@@ -1,1 +1,22 @@
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyFragment extends Fragment {
+
+    public MyFragment() {
+        // Diperlukan konstruktor kosong
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate layout untuk fragment ini
+        return inflater.inflate(R.layout.fragment_my, container, false);
+    }
+}
 
