2ef225f87dd69e1b05cf3d1ae66eb001e9846f61
Jul's <57309553+MyLovelyKitten@users.noreply.github.com>
2023-07-14 22:27:51 +0700

    Update style.java

--- Bab 10/style.java
+++ Bab 10/style.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class tissa.MainActivity extends AppCompatActivity {
 
     Button button1, button2, button3;
 
