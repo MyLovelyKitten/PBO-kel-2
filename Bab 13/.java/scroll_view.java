55f1bf36081b7be4476bf6b43054b1fce286c940
Jul's <57309553+MyLovelyKitten@users.noreply.github.com>
2023-07-14 22:45:15 +0700

    Update scroll_view.java

--- Bab 13/.java/scroll_view.java
+++ Bab 13/.java/scroll_view.java
@@ -1,1 +1,25 @@
+import android.os.Bundle;
+import androidx.appcombat.app.AppCompatActivity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout linearLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        linearLayout = findViewById(R.id.linear_layout);
+
+        // Membuat TextView secara dinamis
+        for (int i = 0; i < 20; i++) {
+            TextView textView = new TextView(this);
+            textView.setText("Item " + (i + 1));
+            linearLayout.addView(textView);
+        }
+    }
+}
 
