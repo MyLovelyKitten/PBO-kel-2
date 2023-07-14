491183f766e447a9ec03288b093e7bca5f40bb06
Jul's <57309553+MyLovelyKitten@users.noreply.github.com>
2023-07-14 22:19:53 +0700

    Update intent.java

--- Bab 12/intent.java
+++ Bab 12/intent.java
@@ -1,1 +1,28 @@
+package com.example.tes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnOpenActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnOpenActivity = findViewById(R.id.btn_open_activity);
+        btnOpenActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
 
