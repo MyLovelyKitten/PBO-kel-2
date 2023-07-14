8bced22a21e0ace520a0b1111d84679892c50364
Jul's <57309553+MyLovelyKitten@users.noreply.github.com>
2023-07-14 22:27:19 +0700

    Update style.java

--- Bab 10/style.java
+++ Bab 10/style.java
@@ -1,1 +1,25 @@
+package com.example.tes;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1, button2, button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.testing_satu);
+        button2 = findViewById(R.id.testing_dua);
+        button3 = findViewById(R.id.testing_tiga);
+    }
+}
 
