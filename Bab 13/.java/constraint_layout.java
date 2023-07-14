8ba1d605e93e870d299c315a89b40f12bdbaa80f
Jul's <57309553+MyLovelyKitten@users.noreply.github.com>
2023-07-14 22:42:41 +0700

    Update constraint_layout.java

--- Bab 13/.java/constraint_layout.java
+++ Bab 13/.java/constraint_layout.java
@@ -1,6 +1,6 @@
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
-import android.appcombat.app.AppCompatActivity;
+import androidx.appcombat.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.TextView;
 
