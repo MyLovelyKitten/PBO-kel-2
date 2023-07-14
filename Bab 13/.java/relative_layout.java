b5e28ce40da2257ca0f7c7f7fa65ba15708505ad
Jul's <57309553+MyLovelyKitten@users.noreply.github.com>
2023-07-14 22:44:06 +0700

    Update relative_layout.java

--- Bab 13/.java/relative_layout.java
+++ Bab 13/.java/relative_layout.java
@@ -1,1 +1,45 @@
+import android.os.Bundle;
+import androidx.appcombat.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RelativeLayout relativeLayout;
+    private TextView textView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        relativeLayout = findViewById(R.id.relative_layout);
+        textView = new TextView(this);
+        textView.setText("Hello, RelativeLayout!");
+        button = new Button(this);
+        button.setText("Click Me");
+
+        RelativeLayout.LayoutParams textParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        textParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        textParams.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT
+        );
+        buttonParams.addRule(RelativeLayout.BELOW, textView.getId());
+        buttonParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+        textView.setLayoutParams(textParams);
+        button.setLayoutParams(buttonParams);
+
+        relativeLayout.addView(textView);
+        relativeLayout.addView(button);
+    }
+}
 
