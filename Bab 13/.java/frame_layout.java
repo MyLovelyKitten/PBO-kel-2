73034f707754d492500c4586e6b59ece9d1f0539
Jul's <57309553+MyLovelyKitten@users.noreply.github.com>
2023-07-14 22:39:35 +0700

    Update frame_layout.java

--- Bab 13/.java/frame_layout.java
+++ Bab 13/.java/frame_layout.java
@@ -1,1 +1,22 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FrameLayout frameLayout;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        frameLayout = findViewById(R.id.frame_layout);
+        imageView = new ImageView(this);
+        imageView.setImageResource(R.drawable.my_image);
+        frameLayout.addView(imageView);
+    }
+}
 
