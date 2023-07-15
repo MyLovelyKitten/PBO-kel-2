c74b999ca30cef6655c5ecac7aa59bfe8a69b9e3
Favianeryramadhani <129515172+Favianeryramadhani@users.noreply.github.com>
2023-07-15 07:26:54 +0700

    Add files via upload

---
+++ Bab 6/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.tutorialpercobaan1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pindah(View view) {
+        Intent intent = new Intent(MainActivity.this,SecondMainActivity.class);
+        startActivity(intent);
+    }
+}
